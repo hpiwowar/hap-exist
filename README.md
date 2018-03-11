a964eabd9b4e9e17d9f25f69660bc326ba1d3064
Heather Piwowar <hpiwowar@gmail.com>
2018-03-11 15:19:59 -0700

    works

---
+++ README.md
@@ -0,0 +1,11 @@
+Playing with @ExistApp,❤️. 
+But I want to correlate w my hormone cycle.  
+So I estimated my progesterone,testosterone,estrogen and used API to insert as if my instagram post,likes,comments stats.  Let's see what correlates!  :) 
+
+see what it looks like when it is inserted:
+https://i.imgur.com/MqDwAM0.png
+
+Data is from "Hormonal characteristics of the human menstrual cycle throughout reproductive life." J Clin Invest. 1975;55(4):699-706. doi:10.1172/JCI107979.
+Figure 3: "The mean and range of serum LH, FSH, E., and P in five women, age 40-41, are compared to the mean +2 SEM in 10 cycles in women aged 18-30."
+Testosterone from a variety of sources, estimated.
+
--- mycode.py
+++ mycode.py
@@ -1,30 +1,92 @@
+import os
+import datetime
+from dateutil import parser
+
+from exist import Exist
+
+exist = Exist(os.getenv("EXIST_CLIENT_ID"), os.getenv("EXIST_CLIENT_SECRET"), os.getenv("EXIST_ACCESS_TOKEN"))
+print (exist.user())
+# print (exist.attributes())
+# print(exist.correlations())
+
+# typo "arquire" is in the library
+exist.arquire_attributes([{"name":"instagram_likes", "active":True}])
+exist.arquire_attributes([{"name":"instagram_comments", "active":True}])
+exist.arquire_attributes([{"name":"instagram_posts", "active":True}])
+
+print [a.label for a in exist.owned_attributes()]
+[str(a) for a in exist.owned_attributes()]
+
+
+# Data is from "Hormonal characteristics of the human menstrual cycle throughout reproductive life." J Clin Invest. 1975;55(4):699-706. doi:10.1172/JCI107979
+# Figure 3: "The mean and range of serum LH, FSH, E., and P in five women, age 40-41, are compared to the mean +2 SEM in 10 cycles in women aged 18-30."
+# testosterone from a variety of sources, estimated
 # ovulation is right after the high day
-data = [
-    {'est': 40, 'prog': 0, 'test': 0},
-    {'est': 40, 'prog': 0, 'test': 0},
-    {'est': 45, 'prog': 0, 'test': 0},
-    {'est': 50, 'prog': 0, 'test': 0},
-    {'est': 70, 'prog': 0, 'test': 0},
-    {'est': 90, 'prog': 0, 'test': 0},
-    {'est': 110, 'prog': 0, 'test': 0},
-    {'est': 130, 'prog': 0, 'test': 0},
-    {'est': 150, 'prog': 0, 'test': 0},
-    {'est': 190, 'prog': 1, 'test': 0},
-    {'est': 210, 'prog': 1, 'test': 2},
-    {'est': 210, 'prog': 1, 'test': 6},
-    {'est': 300, 'prog': 2, 'test': 9},
-    {'est': 400, 'prog': 3, 'test': 10},
-    {'est': 120, 'prog': 4, 'test': 9},
-    {'est': 100, 'prog': 10, 'test': 6},
-    {'est': 100, 'prog': 22, 'test': 2},
-    {'est': 110, 'prog': 26, 'test': 0},
-    {'est': 110, 'prog': 30, 'test': 0},
-    {'est': 120, 'prog': 28, 'test': 0},
-    {'est': 130, 'prog': 25, 'test': 0},
-    {'est': 170, 'prog': 22, 'test': 0},
-    {'est': 160, 'prog': 25, 'test': 0},
-    {'est': 120, 'prog': 23, 'test': 0},
-    {'est': 100, 'prog': 17, 'test': 0},
-    {'est': 80, 'prog': 13, 'test': 0},
-    {'est': 60, 'prog': 8, 'test': 0}
+# assumes 27 day cycle
+
+start_dates_strings = [
+    "2017-11-23",
+    "2017-12-20",
+    "2018-01-17",
+    "2018-02-14"
+]
+start_dates = [parser.parse(x) for x in start_dates_strings]
+
+hormone_levels_by_day = [
+    {"estrogen": 40, "progesterone": 0, "testosterone": 0},  # day 0
+    {"estrogen": 40, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 45, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 50, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 70, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 90, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 110, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 130, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 150, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 190, "progesterone": 1, "testosterone": 0},
+    {"estrogen": 210, "progesterone": 1, "testosterone": 2},  # offset 10
+    {"estrogen": 210, "progesterone": 1, "testosterone": 6},
+    {"estrogen": 300, "progesterone": 2, "testosterone": 9},
+    {"estrogen": 400, "progesterone": 3, "testosterone": 10},
+    {"estrogen": 120, "progesterone": 4, "testosterone": 9},  #ovulation day offset 14
+    {"estrogen": 100, "progesterone": 10, "testosterone": 6},
+    {"estrogen": 100, "progesterone": 22, "testosterone": 2},
+    {"estrogen": 110, "progesterone": 26, "testosterone": 0},
+    {"estrogen": 110, "progesterone": 30, "testosterone": 0},
+    {"estrogen": 120, "progesterone": 28, "testosterone": 0},
+    {"estrogen": 130, "progesterone": 25, "testosterone": 0},  # offset 20
+    {"estrogen": 170, "progesterone": 22, "testosterone": 0},
+    {"estrogen": 160, "progesterone": 25, "testosterone": 0},
+    {"estrogen": 120, "progesterone": 23, "testosterone": 0},
+    {"estrogen": 100, "progesterone": 17, "testosterone": 0},
+    {"estrogen": 80, "progesterone": 13, "testosterone": 0},
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 26
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 27
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 28
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0}  # offset 29
     ]
+
+
+def update_exist(my_day, levels):
+    my_day_str = my_day.isoformat()[0:10]
+    print "saving {} for {}".format(levels, my_day_str)
+    response = exist.update_attributes([
+        {"name": "instagram_likes", "date": my_day_str, "value":levels["estrogen"]},
+        {"name": "instagram_comments", "date": my_day_str, "value":levels["progesterone"]},
+        {"name": "instagram_posts", "date": my_day_str, "value":levels["testosterone"]}
+    ])
+
+def run():
+    my_day = parser.parse("2017-11-24")
+    previous_period_start = None
+    while my_day <= datetime.datetime.now():
+        for start_date in sorted(start_dates):
+            if start_date <= my_day:
+                previous_period_start = start_date
+        days_into_period = (my_day - previous_period_start).days
+        print "days_into_period", days_into_period
+        levels = hormone_levels_by_day[days_into_period]
+        update_exist(my_day, levels)
+        my_day = my_day + datetime.timedelta(days=1)
+
+
+run()
