efdf388f6264d316a1b21185e884659a0221529f
Heather Piwowar <hpiwowar@gmail.com>
2018-03-28 05:43:01 -0700

    shift so that peak of estrogen lines up better with day before ovulation

--- mycode.py
+++ mycode.py
@@ -9,7 +9,7 @@ from exist import Exist
 
 
 exist = Exist(os.getenv("EXIST_CLIENT_ID"), os.getenv("EXIST_CLIENT_SECRET"), os.getenv("EXIST_ACCESS_TOKEN"))
-print (exist.user())
+# print (exist.user())
 # print (exist.attributes())
 # print(exist.correlations())
 
@@ -28,9 +28,7 @@ print [a.label for a in exist.owned_attributes()]
 # assumes 27 day cycle
 
 hormone_levels_by_day = [
-    {"estrogen": 40, "progesterone": 0, "testosterone": 0},  # day 0, first day of bleeding
-    {"estrogen": 40, "progesterone": 0, "testosterone": 0},
-    {"estrogen": 45, "progesterone": 0, "testosterone": 0},
+    {"estrogen": 45, "progesterone": 0, "testosterone": 0}, # day 0, first day of bleeding
     {"estrogen": 50, "progesterone": 0, "testosterone": 0},
     {"estrogen": 70, "progesterone": 0, "testosterone": 0},
     {"estrogen": 90, "progesterone": 0, "testosterone": 0},
@@ -38,26 +36,28 @@ hormone_levels_by_day = [
     {"estrogen": 130, "progesterone": 0, "testosterone": 0},
     {"estrogen": 150, "progesterone": 0, "testosterone": 0},
     {"estrogen": 190, "progesterone": 1, "testosterone": 0},
-    {"estrogen": 210, "progesterone": 1, "testosterone": 2},  # offset 10
+    {"estrogen": 210, "progesterone": 1, "testosterone": 2},
     {"estrogen": 210, "progesterone": 1, "testosterone": 6},
     {"estrogen": 300, "progesterone": 2, "testosterone": 9},
     {"estrogen": 400, "progesterone": 3, "testosterone": 10},
-    {"estrogen": 120, "progesterone": 4, "testosterone": 9},  #ovulation day offset 14
+    {"estrogen": 120, "progesterone": 4, "testosterone": 9},  #ovulation day offset 12
     {"estrogen": 100, "progesterone": 10, "testosterone": 6},
     {"estrogen": 100, "progesterone": 22, "testosterone": 2},
     {"estrogen": 110, "progesterone": 26, "testosterone": 0},
     {"estrogen": 110, "progesterone": 30, "testosterone": 0},
     {"estrogen": 120, "progesterone": 28, "testosterone": 0},
-    {"estrogen": 130, "progesterone": 25, "testosterone": 0},  # offset 20
+    {"estrogen": 130, "progesterone": 25, "testosterone": 0},
     {"estrogen": 170, "progesterone": 22, "testosterone": 0},
-    {"estrogen": 160, "progesterone": 25, "testosterone": 0},
+    {"estrogen": 160, "progesterone": 25, "testosterone": 0},  # offset 20
     {"estrogen": 120, "progesterone": 23, "testosterone": 0},
     {"estrogen": 100, "progesterone": 17, "testosterone": 0},
     {"estrogen": 80, "progesterone": 13, "testosterone": 0},
-    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 26
-    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 27
-    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 28
-    {"estrogen": 60, "progesterone": 8, "testosterone": 0}  # offset 29
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},  # offset 26, 27th day of period where bleeding is day 1
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0},
+    {"estrogen": 60, "progesterone": 8, "testosterone": 0}
     ]
 
 period_start_date_strings = [
